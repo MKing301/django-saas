04705070d426dc0af2f53ed60100acfe53d29515
MKING301 <33675411+MKing301@users.noreply.github.com>
2024-09-02 10:38:42 -0400

    Add course url patterns

--- CoursePlatform/payment/urls.py
+++ CoursePlatform/payment/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
 
 urlpatterns = [
-
+    path('<int:course_id>/', views.create_checkout_session, name='create_checkout_session'),
+    path('course_success/', views.course_success, name='course_success'),
+    path('course_cancel/', views.course_cancel, name='course_cancel'),
+    path('stripe/webhook', views.stripe_webhook, name='stripe_webhook'),
 ]

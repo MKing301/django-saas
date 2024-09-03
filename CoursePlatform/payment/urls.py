8ca38302cbc45821c856e3096fc6162789ae8926
MKING301 <33675411+MKing301@users.noreply.github.com>
2024-09-03 16:38:31 -0400

    Update cancel urp pattern

--- CoursePlatform/payment/urls.py
+++ CoursePlatform/payment/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('<int:course_id>/', views.create_checkout_session, name='create_checkout_session'),
     path('course_success/', views.course_success, name='course_success'),
-    path('course_cancel/', views.course_cancel, name='course_cancel'),
+    path('course_cancel', views.course_cancel, name='course_cancel'),
     path('stripe/webhook', views.stripe_webhook, name='stripe_webhook'),
 ]
